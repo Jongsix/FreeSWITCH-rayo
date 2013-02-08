f838926462bb076c0d66fb3df207d0ad8531623b
Chris Rienzo <chris.rienzo@grasshopper.com>
2013-02-08 17:16:49 -0500

    mod_rayo: bind to XMPP port and accept connections

--- src/mod/event_handlers/mod_rayo/mod_rayo.c
+++ src/mod/event_handlers/mod_rayo/mod_rayo.c
@@ -28,11 +28,13 @@
  *
  */
 #include <switch.h>
+#include <iksemel.h>
+
+#define XMPP_CLIENT_PORT "5222"
 
 SWITCH_MODULE_SHUTDOWN_FUNCTION(mod_rayo_shutdown);
-SWITCH_MODULE_RUNTIME_FUNCTION(mod_rayo_runtime);
 SWITCH_MODULE_LOAD_FUNCTION(mod_rayo_load);
-SWITCH_MODULE_DEFINITION(mod_rayo, mod_rayo_load, mod_rayo_shutdown, mod_rayo_runtime);
+SWITCH_MODULE_DEFINITION(mod_rayo, mod_rayo_load, mod_rayo_shutdown, NULL);
 
 /**
  * Module state
@@ -43,13 +45,199 @@ static struct {
 } globals;
 
 /**
+ * A server listening for clients
+ */
+struct rayo_server {
+	switch_memory_pool_t *pool;
+	char *addr;
+	switch_port_t port;
+	switch_socket_t *socket;
+};
+
+/**
+ * A connection
+ */
+struct rayo_connection {
+	char *addr;
+	switch_port_t port;
+	char *remote_addr;
+	switch_port_t remote_port;
+	switch_memory_pool_t *pool;
+	switch_socket_t *socket;
+};
+
+/**
+ * Handles Rayo stream
+ */
+static void *SWITCH_THREAD_FUNC rayo_connection_thread(switch_thread_t *thread, void *obj)
+{
+	struct rayo_connection *connection = (struct rayo_connection *)obj;
+	switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_DEBUG, "New connection\n");	
+	/* TBD */
+	switch_socket_shutdown(connection->socket, SWITCH_SHUTDOWN_READWRITE);
+	switch_socket_close(connection->socket);
+	switch_core_destroy_memory_pool(&connection->pool);
+	switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_DEBUG, "Connection closed\n");
+	return NULL;
+}
+
+
+/**
+ * Listens for new Rayo client connections
+ */
+static void *SWITCH_THREAD_FUNC rayo_server_thread(switch_thread_t *thread, void *obj)
+{
+	struct rayo_server *server = (struct rayo_server *)obj;
+	uint32_t errs = 0;
+
+	/* bind to XMPP port */
+	while (!globals.shutdown) {
+		switch_status_t rv;
+		switch_sockaddr_t *sa;
+		rv = switch_sockaddr_info_get(&sa, server->addr, SWITCH_UNSPEC, server->port, 0, server->pool);
+		if (rv)
+			goto fail;
+		rv = switch_socket_create(&server->socket, switch_sockaddr_get_family(sa), SOCK_STREAM, SWITCH_PROTO_TCP, server->pool);
+		if (rv)
+			goto sock_fail;
+		rv = switch_socket_opt_set(server->socket, SWITCH_SO_REUSEADDR, 1);
+		if (rv)
+			goto sock_fail;
+#ifdef WIN32
+		/* Enable dual-stack listening on Windows (if the listening address is IPv6), it's default on Linux */
+		if (switch_sockaddr_get_family(sa) == AF_INET6) {
+			rv = switch_socket_opt_set(server->socket, 16384, 0);
+			if (rv) goto sock_fail;
+		}
+#endif
+		rv = switch_socket_bind(server->socket, sa);
+		if (rv)
+			goto sock_fail;
+		rv = switch_socket_listen(server->socket, 5);
+		if (rv)
+			goto sock_fail;
+		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_DEBUG, "Rayo server listening on %s:%u\n", server->addr, server->port);
+
+		break;
+   sock_fail:
+		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "Socket Error! Rayo server could not listen on %s:%u\n", server->addr, server->port);
+		switch_yield(100000);
+	}
+
+	/* Listen for XMPP client connections */
+	while (!globals.shutdown) {
+		switch_socket_t *socket = NULL;
+		switch_memory_pool_t *pool = NULL;
+		switch_status_t rv;
+		
+		if (switch_core_new_memory_pool(&pool) != SWITCH_STATUS_SUCCESS) {
+			switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "Failed to create memory pool for new client connection!\n");
+			goto fail;
+		}
+
+		if ((rv = switch_socket_accept(&socket, server->socket, pool))) {
+			switch_core_destroy_memory_pool(&pool);
+			if (globals.shutdown) {
+				switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_NOTICE, "Shutting Down\n");
+				goto end;
+			} else {
+				/* I wish we could use strerror_r here but its not defined everywhere =/ */
+				switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "Socket Error [%s]\n", strerror(errno));
+				if (++errs > 100) {
+					goto end;
+				}
+			}
+		} else { /* got a new connection */
+			switch_thread_t *thread;
+			switch_threadattr_t *thd_attr = NULL;
+			struct rayo_connection *connection;
+			
+			errs = 0;
+			
+			/* start connection thread */
+			if (!(connection = switch_core_alloc(pool, sizeof(*connection)))) {
+				switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "Memory Error\n");
+				switch_socket_shutdown(socket, SWITCH_SHUTDOWN_READWRITE);
+				switch_socket_close(socket);
+				switch_core_destroy_memory_pool(&pool);
+				break;
+			}
+			connection->pool = pool;
+			connection->socket = socket;
+			connection->addr = ""; /* TODO */
+			connection->port = 0; /* TODO */
+			connection->remote_addr = ""; /* TODO */
+			connection->remote_port = 0; /* TODO */
+			switch_threadattr_create(&thd_attr, connection->pool);
+			switch_threadattr_detach_set(thd_attr, 1);
+			switch_threadattr_stacksize_set(thd_attr, SWITCH_THREAD_STACKSIZE);
+			switch_thread_create(&thread, thd_attr, rayo_connection_thread, connection, connection->pool);
+		}
+	}
+
+  end:
+
+	/* shutdown server */
+	switch_socket_shutdown(server->socket, SWITCH_SHUTDOWN_READWRITE);
+	switch_socket_close(server->socket);
+
+	if (server->pool) {
+		switch_core_destroy_memory_pool(&server->pool);
+	}
+
+fail:
+	switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_DEBUG, "Rayo server %s:%u thread done\n", server->addr, server->port);
+	return NULL;
+}
+
+/**
+ * Add a new server for Rayo client connections.
+ * @param addr the IP address
+ * @param port the port
+ */
+static switch_status_t add_rayo_server(char *addr, char *port)
+{
+	switch_memory_pool_t *pool;
+	struct rayo_server *new_server = NULL;
+	switch_thread_t *thread;
+	switch_threadattr_t *thd_attr = NULL;
+
+	if (zstr(addr)) {
+		return SWITCH_STATUS_FALSE;
+	}
+
+	if (zstr(port)) {
+		port = XMPP_CLIENT_PORT;
+	}
+
+	if (switch_core_new_memory_pool(&pool) != SWITCH_STATUS_SUCCESS) {
+		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "Failed to create memory pool!\n");
+		return SWITCH_STATUS_FALSE;
+	}
+
+	new_server = switch_core_alloc(pool, sizeof(*new_server));
+	new_server->pool = pool;
+	new_server->addr = switch_core_strdup(new_server->pool, addr);
+	new_server->port = atoi(port);
+
+	/* start the server thread */
+	switch_threadattr_create(&thd_attr, new_server->pool);
+	switch_threadattr_detach_set(thd_attr, 1);
+	switch_threadattr_stacksize_set(thd_attr, SWITCH_THREAD_STACKSIZE);
+	switch_thread_create(&thread, thd_attr, rayo_server_thread, new_server, new_server->pool);
+	
+	return SWITCH_STATUS_SUCCESS;
+}
+
+/**
  * Process module XML configuration
  * @param pool memory pool to allocate from
  * @return SWITCH_STATUS_SUCCESS on successful (re)configuration
  */
-static switch_status_t do_config(switch_memory_pool_t *pool) {
+static switch_status_t do_config(switch_memory_pool_t *pool)
+{
 	char *cf = "rayo.conf";
-	switch_xml_t cfg, xml, param, settings;
+	switch_xml_t cfg, xml;
 	switch_status_t status = SWITCH_STATUS_SUCCESS;
 
 	switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_DEBUG, "Configuring module\n");
@@ -59,16 +247,35 @@ static switch_status_t do_config(switch_memory_pool_t *pool) {
 	}
 
 	/* get params */
-	settings = switch_xml_child(cfg, "settings");
-	if (settings) {
-		for (param = switch_xml_child(settings, "param"); param; param = param->next) {
-			char *var = (char *) switch_xml_attr_soft(param, "name");
-			char *val = (char *) switch_xml_attr_soft(param, "value");
-			switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_DEBUG, "param: %s = %s\n", var, val);
-			if (!strcasecmp(var, "foo")) {
-				/* TODO remove */
-			} else {
-				switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_WARNING, "Unsupported param: %s\n", var);
+	{
+		switch_xml_t settings = switch_xml_child(cfg, "settings");
+		if (settings) {
+			switch_xml_t param;
+			for (param = switch_xml_child(settings, "param"); param; param = param->next) {
+				char *var = (char *) switch_xml_attr_soft(param, "name");
+				char *val = (char *) switch_xml_attr_soft(param, "value");
+				switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_DEBUG, "param: %s = %s\n", var, val);
+				if (!strcasecmp(var, "foo")) {
+					/* TODO remove */
+				} else {
+					switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_WARNING, "Unsupported param: %s\n", var);
+				}
+			}
+		}
+	}
+
+	/* configure listen addresses */
+	{
+		switch_xml_t listeners = switch_xml_child(cfg, "listeners");
+		if (listeners) {
+			switch_xml_t l;
+			for (l = switch_xml_child(listeners, "listener"); l; l = l->next) {
+				char *val = switch_xml_txt(l);
+				char *port = "";
+				switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_DEBUG, "Listener: %s\n", val);
+				if (add_rayo_server(val, port) != SWITCH_STATUS_SUCCESS) {
+					switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_WARNING, "Failed to create listener: %s\n", val);
+				}
 			}
 		}
 	}
@@ -119,20 +326,6 @@ SWITCH_MODULE_SHUTDOWN_FUNCTION(mod_rayo_shutdown)
 	switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_DEBUG, "Shutdown module\n");
 	globals.shutdown = 1;
 	return SWITCH_STATUS_SUCCESS;
-}
-
-/**
- * Module runtime thread.  Executes for lifetime of module.
- */
-SWITCH_MODULE_RUNTIME_FUNCTION(mod_rayo_runtime)
-{
-	switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_DEBUG, "Runtime thread started\n");
-	while(!globals.shutdown) {
-		/* TODO something interesting */
-		switch_sleep(500 * 1000); /* 500 ms */
-	}
-	switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_DEBUG, "Runtime thread done\n");
-	return SWITCH_STATUS_TERM;
 }
 
 /* For Emacs:
