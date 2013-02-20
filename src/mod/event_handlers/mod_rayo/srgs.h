9e833827e158111674ce0dfbcc50f36acf2fd521
Chris Rienzo <chris.rienzo@grasshopper.com>
2013-02-20 15:39:33 -0500

    mod_rayo: starting <input mode='dtmf'>

---
+++ src/mod/event_handlers/mod_rayo/srgs.h
@@ -0,0 +1,57 @@
+/*
+ * FreeSWITCH Modular Media Switching Software Library / Soft-Switch Application
+ * Copyright (C) 2005-2013, Anthony Minessale II <anthm@freeswitch.org>
+ *
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is FreeSWITCH Modular Media Switching Software Library / Soft-Switch Application
+ *
+ * The Initial Developer of the Original Code is
+ * Anthony Minessale II <anthm@freeswitch.org>
+ * Portions created by the Initial Developer are Copyright (C)
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ * Chris Rienzo <chris.rienzo@grasshopper.com>
+ *
+ * srgs.h -- Transforms SRGS into regex rules
+ *
+ */
+#ifndef SRGS_H
+#define SRGS_H
+
+struct srgs_parser;
+
+enum match_type {
+	MT_NO_MATCH,
+	MT_MATCH,
+	MT_NOT_ENOUGH_INPUT
+};
+
+extern struct srgs_parser *srgs_parser_new(void);
+extern int srgs_parse(struct srgs_parser *parser, const char *document);
+extern enum match_type srgs_match(struct srgs_parser *parser, const char *input);
+extern void srgs_destroy(struct srgs_parser *parser);
+
+#endif
+
+/* For Emacs:
+ * Local Variables:
+ * mode:c
+ * indent-tabs-mode:t
+ * tab-width:4
+ * c-basic-offset:4
+ * End:
+ * For VIM:
+ * vim:set softtabstop=4 shiftwidth=4 tabstop=4
+ */
