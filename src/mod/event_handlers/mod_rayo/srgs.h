d006182a8cc8b3a10f5af740609361e4bfa7cf73
Chris Rienzo <chris.rienzo@grasshopper.com>
2013-03-02 22:48:31 -0500

    mod_rayo: reworked srgs (again) to use PCRE native API

--- src/mod/event_handlers/mod_rayo/srgs.h
+++ src/mod/event_handlers/mod_rayo/srgs.h
@@ -37,12 +37,11 @@ struct srgs_parser;
 enum srgs_match_type {
 	SMT_NO_MATCH,
 	SMT_MATCH,
-	SMT_TIMEOUT
+	SMT_MATCH_PARTIAL
 };
 
 extern struct srgs_parser *srgs_parser_new(switch_memory_pool_t *pool, const char *uuid);
-extern void srgs_reset(struct srgs_parser *parser);
-extern int srgs_parse(struct srgs_parser *parser, const char *document, int input_timeout_ms, int digit_timeout_ms);
+extern int srgs_parse(struct srgs_parser *parser, const char *document);
 extern enum srgs_match_type srgs_match(struct srgs_parser *parser, const char *input);
 
 #endif
